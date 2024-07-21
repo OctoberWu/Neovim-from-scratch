c18f859ee8e763dda2b8f1fbe608728e3f55ee30
John Wu <john.sc.wu.3000@gmail.com>
2024-07-21 12:51:51 +0800

    chore: remove 'mason_tool_installer' setup options

--- lua/john/plugins2/lsp/mason.lua
+++ lua/john/plugins2/lsp/mason.lua
@@ -30,19 +30,12 @@ return {
         "tsserver",
         "html",
         "cssls",
-        "lua_ls",
         "pyright",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
       },
     })
   end,
