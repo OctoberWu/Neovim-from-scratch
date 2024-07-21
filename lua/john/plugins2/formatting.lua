fa68ae5e98f7b4f3ba954202abbc0257cadfe9e4
John Wu <john.sc.wu.3000@gmail.com>
2024-07-21 13:03:19 +0800

    chore: comment part of formatting config to suppress complains

--- lua/john/plugins2/formatting.lua
+++ lua/john/plugins2/formatting.lua
@@ -1,28 +1,29 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  -- event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   config = function()
     local conform = require("conform")
 
     conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      -- formatters_by_ft = {
+      --   javascript = { "prettier" },
+      --   typescript = { "prettier" },
+      --   javascriptreact = { "prettier" },
+      --   typescriptreact = { "prettier" },
+      --   css = { "prettier" },
+      --   html = { "prettier" },
+      --   json = { "prettier" },
+      --   yaml = { "prettier" },
+      --   markdown = { "prettier" },
+      --   lua = { "stylua" },
+      --   python = { "isort", "black" },
+      -- },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   async = false,
+      --   timeout_ms = 1000,
+      -- },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
